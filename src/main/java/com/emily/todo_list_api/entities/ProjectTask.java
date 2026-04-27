cb70e579e3a23300241099b0fda21a4dad044b9f
Emily Quevedo <emilyqdrosa@gmail.com>
2026-04-27 01:30:00 -0300

    adicionando task de projeto

--- src/main/java/com/emily/todo_list_api/entities/ProjectTask.java
+++ src/main/java/com/emily/todo_list_api/entities/ProjectTask.java
@@ -1,4 +1,29 @@
 package com.emily.todo_list_api.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "todo_list_api", name = "project_tasks")
 public class ProjectTask extends BaseTask{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToMany
+    @JoinTable(
+            name = "task_responsibles",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> responsibles;
 }
