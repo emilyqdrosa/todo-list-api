40e0e2f7afba3c8aaa0006546d828210a96dce6d
Emily Quevedo <emilyqdrosa@gmail.com>
2026-04-26 22:07:43 -0300

    entities criadas

---
+++ src/main/java/com/emily/todo_list_api/entities/BaseTask.java
@@ -0,0 +1,4 @@
+package com.emily.todo_list_api.entities;
+
+public abstract class BaseTask {
+}
---
+++ src/main/java/com/emily/todo_list_api/entities/IndividualTask.java
@@ -0,0 +1,4 @@
+package com.emily.todo_list_api.entities;
+
+public class IndividualTask extends BaseTask {
+}
