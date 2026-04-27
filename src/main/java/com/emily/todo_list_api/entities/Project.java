91824b0d4e38276f52141e8d853e0e7d9a1c6876
Emily Quevedo <emilyqdrosa@gmail.com>
2026-04-27 01:39:28 -0300

    adicionadno entidade projeto

--- src/main/java/com/emily/todo_list_api/entities/Project.java
+++ src/main/java/com/emily/todo_list_api/entities/Project.java
@@ -1,4 +1,52 @@
 package com.emily.todo_list_api.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "todo_list_api", name = "projects")
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_project", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotBlank(message = "O nome do projeto é obrigatório")
+    @Size(min = 3, max = 255)
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_members",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> members;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<ProjectTask> tasks;
 }
