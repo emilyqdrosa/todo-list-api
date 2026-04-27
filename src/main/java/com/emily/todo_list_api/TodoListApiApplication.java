ac60439dbe74334be9a54ca2cad34f5234daf034
Emily Quevedo <emilyqdrosa@gmail.com>
2026-04-26 22:01:36 -0300

    configs

--- src/main/java/com/emily/todo_list_api/TodoListApiApplication.java
+++ src/main/java/com/emily/todo_list_api/TodoListApiApplication.java
@@ -1,9 +1,7 @@
 package com.emily.todo_list_api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class TodoListApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApiApplication.class, args);
